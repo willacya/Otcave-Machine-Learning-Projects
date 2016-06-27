31f2a7742a2877737cca8ac1a3d127652dbc9da3
willacya <andrewwillacy@gmail.com>
2016-06-27 13:38:51 +0100

    Hoping to upload machine learning octave proj. to git

---
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -0,0 +1,39 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+
+h = sigmoid(X * theta);
+
+theta(1) = 0;
+minus_y = -y'*log(h);
+one_minus_y = (1-y)'*log(1-h);
+cost_total = (sum(minus_y - one_minus_y))/m;
+reg = (theta'*theta)*(lambda/(2*m));
+
+J = cost_total + reg;
+
+
+
+grad = ((X'*(h-y))/m)+((lambda/m)*theta);
+
+
+
+
+% =============================================================
+
+end
